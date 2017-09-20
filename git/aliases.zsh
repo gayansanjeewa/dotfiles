2ba37e715eac478d37810b9a9dd80988d8020787
Toni Van de Voorde <toni.vdv@gmail.com>
2017-09-20 09:27:46 +0200

    add gpul enhancement

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,15 +7,4 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gpull='git pull --prune --rebase'
-alias gpul=gpull
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
+alias glog="git glog"
